ed38c800a687ebeffa27d53e1705e12a204d256d
Fred <voiaka_89@mail.ru>
2022-07-07 16:53:07 +0300

    Lk

--- public/js/applications.js
+++ public/js/applications.js
@@ -1,6 +1,17 @@
-// const baseModule = require("hbs");
+// // const baseModule = require("hbs");
+// const authButton = document.querySelector('.body');
+// authButton.addEventListener('click', async (e) => {
+//   if (e.target.type === 'submit') {
+//     e.preventDefault();
+//     const response = await fetch('/singup', {
+//       method: 'post',
+//       headers: {
+//         'Content-type': 'application/json',
+//       },
+//       body: JSON.stringify({ }),
+//     });
+//     if (response.ok) {
 
-// baseModule.addEventListener('click', async (e) => {
-//   e.preventDefault()
-
-// })
+//     }
+//   }
+// });
--- routes/index.router.js
+++ routes/index.router.js
@@ -33,7 +33,7 @@ router.post('/signup', async (req, res) => {
     }
 
     req.session.userId = newUser.id;
-
+console.log(req.session.userId );
     return res.redirect('/');
   } catch (error) {
     return res.sendStatus(401);
