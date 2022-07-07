33d6de6debb8cf71cd97154e702bb596a41d0df7
Countrvl <countrvl@gmail.com>
2022-07-07 10:08:41 +0300

    init cart

--- routes/cart.router.js
+++ routes/cart.router.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.render('test_back_index');
+  res.redirect('/');
 });
+
+// router.get('/add', (req, res) => {
+  
+// });
+// router.get('/del/:id', (req, res) => {
+
+// });
+
+// router.get('/cart', (req, res) => {
+
+// });
+
 
 module.exports = router;
