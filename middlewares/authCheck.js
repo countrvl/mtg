27bfae65096c63fcab4e8afe29087bafe113ae0b
Countrvl <countrvl@gmail.com>
2022-07-08 14:29:24 +0300

    fix LK, authCheck

--- middlewares/authCheck.js
+++ middlewares/authCheck.js
@@ -2,8 +2,8 @@ const authCheck = (req, res, next) => {
   if (req.session?.userName) {
     next();
   } else {
-    res.redirect('/signup');
-    // res.sendStatus(418);
+    res.sendStatus(403);
+    // res.status(418).redirect('/signup');    
   }
 };
 
